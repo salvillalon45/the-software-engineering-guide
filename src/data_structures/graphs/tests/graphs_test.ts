84d4176b01e4e6844a3e3e880781e9d3d2ed42e1
Salvador Villalon Jr <salvadorvillalon54@gmail.com>
2024-08-12 11:47:11 -0700

    almost done with bfs!

--- src/data_structures/graphs/tests/graphs_test.ts
+++ src/data_structures/graphs/tests/graphs_test.ts
@@ -3,7 +3,7 @@
 const exBFSGraph = [
 	[0, 1, 0, 0],
 	[1, 0, 1, 0],
-	[0, 1, 0, 1],
+	[0, 1, 0, 0],
 	[0, 0, 0, 0]
 ];
 const startingNode = 1;
@@ -14,8 +14,12 @@ function bfsHelper(exBFSGraph: Array<Array<number>>, startingNode: number) {
 	const queue = [startingNode];
 	const graph = exBFSGraph;
 	const visitedNodes = new Set();
-	const distances = {};
-	exBFSGraph.map((array, index) => (distances[index] = Infinity));
+	interface Distances {
+		[x: string]: number;
+	}
+	const distances: Distances = {};
+	exBFSGraph.map((array, index) => (distances[index.toString()] = 0));
+	distances[startingNode] = 0;
 	console.log({ distances });
 
 	while (queue.length > 0) {
@@ -29,11 +33,16 @@ function bfsHelper(exBFSGraph: Array<Array<number>>, startingNode: number) {
 				console.log('going to push to queue');
 				queue.push(index);
 				console.log({ queue });
+				let res = distances[index];
+				console.log({ res });
+				res += 1;
+				distances[index] = res;
+				console.log({ distances });
 			}
 		});
 	}
 
-	console.log({ queue, visitedNodes });
+	console.log({ queue, visitedNodes, distances });
 }
 
 bfsHelper(exBFSGraph, startingNode);
